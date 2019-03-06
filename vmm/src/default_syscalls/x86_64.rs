c885739bf348962e5aa50afb0d7b4dd0eac9dd48
Alexandra Iordache <aghecen@amazon.com>
2019-03-06 19:30:58 +0200

    seccomp: allow madvise(*, *, MADV_DONTNEED)
    The musl allocator calls it to punch holes in large chunks of allocated
    memory:
    https://elixir.bootlin.com/musl/v1.1.20/source/src/malloc/malloc.c#L501
    
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- vmm/src/default_syscalls/x86_64.rs
+++ vmm/src/default_syscalls/x86_64.rs
@@ -23,6 +23,7 @@ pub const ALLOWED_SYSCALLS: &[i64] = &[
     libc::SYS_futex,
     libc::SYS_ioctl,
     libc::SYS_lseek,
+    libc::SYS_madvise,
     libc::SYS_mmap,
     libc::SYS_munmap,
     libc::SYS_open,
@@ -240,6 +241,20 @@ pub fn default_context() -> Result<SeccompFilterContext, Error> {
             (
                 libc::SYS_lseek,
                 (0, vec![SeccompRule::new(vec![], SeccompAction::Allow)]),
+            ),
+            (
+                libc::SYS_madvise,
+                (
+                    0,
+                    vec![SeccompRule::new(
+                        vec![SeccompCondition::new(
+                            2,
+                            SeccompCmpOp::Eq,
+                            libc::MADV_DONTNEED as u64,
+                        )?],
+                        SeccompAction::Allow,
+                    )],
+                ),
             ),
             (
                 libc::SYS_mmap,
